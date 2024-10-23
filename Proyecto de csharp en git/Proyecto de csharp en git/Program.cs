924f6e5a70157d0a46c8a036028d249fece03e76
Alan <Gova080521hgtnzla7@cbtis217.edu.mx>
2024-10-23 15:45:20 +0200

    inicialisacion de proyecto

---
+++ Proyecto de csharp en git/Proyecto de csharp en git/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2-PC18
+ * Date: 23/10/2024
+ * Time: 03:44 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace Proyecto_de_csharp_en_git
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
